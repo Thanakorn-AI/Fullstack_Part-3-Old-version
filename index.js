e4ecb0cfdd1d7ae9409a81de88d6d2bf84e7e6e8
Duke <dukethanakorn@gmail.com>
2025-02-13 21:24:05 +0700

    Completed "3.2: Phonebook backend step 2"

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 
+const persons = [
+    { id: "1", name: "Arto Hellas", number: "040-123456" },
+    { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
+    { id: "3", name: "Dan Abramov", number: "12-43-234345" },
+    { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
+];
+
 app.get('/api/persons', (req, res) => {
-    res.json([
-        { id: "1", name: "Arto Hellas", number: "040-123456" },
-        { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
-        { id: "3", name: "Dan Abramov", number: "12-43-234345" },
-        { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
-    ]);
+    res.json(persons);
 });
+
+app.get('/info', (req, res) => {
+    const numberOfPeople = persons.length;
+    const date = new Date();
+    res.send(`Phonebook has info for ${numberOfPeople} people<br/>${date}`);
+});
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
