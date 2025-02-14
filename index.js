e99677de8ca51607efdc2a12f846df02f23ea5bc
Thanakorn Kitviriya <dukethanakorn@gmail.com>
2025-02-14 08:23:46 +0700

    Completed "3.4: Phonebook backend step 4"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
     { id: "1", name: "Arto Hellas", number: "040-123456" },
     { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
     { id: "3", name: "Dan Abramov", number: "12-43-234345" },
@@ -28,6 +28,21 @@ app.get('/api/persons/:id', (req, res) => {
         res.json(person);
     } else {
         res.status(404).json({ error: 'Person not found' });
+    }
+});
+
+
+// Route to delete a single person by ID
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const initialLength = persons.length;
+    // Filter the persons array to remove the person with the matching id
+    persons = persons.filter(person => person.id !== id);
+
+    if (persons.length < initialLength) {
+        res.status(204).end(); // Send a 204 No Content response if the deletion was successful
+    } else {
+        res.status(404).json({ error: 'Person not found' }); // Send a 404 Not Found if no person with this id existed
     }
 });
 
